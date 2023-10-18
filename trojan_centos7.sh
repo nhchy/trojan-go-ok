d91808dff33183b8e05ebe39531b6b77e2a4b76d
nhchy <62925206+nhchy@users.noreply.github.com>
2023-10-18 19:53:38 +0800

    Add files via upload

--- trojan_centos7.sh
+++ trojan_centos7.sh
@@ -66,7 +66,7 @@ if [ $real_addr == $local_addr ] ; then
 	#设置伪装站
 	rm -rf /usr/share/nginx/html/*
 	cd /usr/share/nginx/html/
-	wget https://github.com/atrandys/v2ray-ws-tls/raw/master/web.zip
+	wget https://github.com/nhchy/trojan-go-ok/raw/main/web.zip
     	unzip web.zip
 	systemctl restart nginx.service
 	#申请https证书
